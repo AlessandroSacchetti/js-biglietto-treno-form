98fc8c7547abc71d1ee635fbadc667be8c19bec2
Alessandro Sacchetti Benassi <a.sac.ben@gmail.com>
2024-03-29 19:44:44 +0100

    add: initiated js and HTML

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//TODO
+//collegare gli input di HTML con JS
+//trasformare gli elementi in modo tale da poter eseguire le operazioni di calcolo del costo del biglietto e dello sconto applicato
+//stampare in HTML il risultato dei calcoli, possibilmente riportato in una scheda di riepilogo del viaggio
+
+//collegamento input HTML all'input 'submit' del form
+
+const buttonElementSubmit = document.getElementById('submit')
+
+const kmElement = document.getElementById ('km')
+const km = kmElement.value
+
+const ageElement = document.getElementById('age')
+const age = ageElement.value
+
+buttonElementSubmit.addEventListener ('click', function () {
+
+    const km = kmElement.value
+    const age = ageElement.value
+
+})
+
+
